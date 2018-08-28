48b7d20a0eae1651e1ae5ab5988e939933a8e18a
wayleung <610686949@qq.com>
2018-08-28 16:12:43 +0800

    add feign

---
+++ service-feign/src/main/java/com/way/servicefeign/service/SchedualServiceHi.java
@@ -0,0 +1,14 @@
+package com.way.servicefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//@Service
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi{
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
