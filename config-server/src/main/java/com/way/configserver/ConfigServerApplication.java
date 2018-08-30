ebbecf503b78a7302957a3a09b389ba2924cc29d
wayleung <610686949@qq.com>
2018-08-30 13:56:56 +0800

    add distributed config server

--- config-server/src/main/java/com/way/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/way/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.way.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 /**
  * #http请求地址和资源文件映射如下:
  * #/{application}/{profile}[/{label}]
